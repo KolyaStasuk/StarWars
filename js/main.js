126782995bd1836df3ece349c28d6b082c79de52
Kolya <stasukkola739@gmail.com>
2022-06-28 19:17:42 +0300

    First->Last

---
+++ js/main.js
@@ -0,0 +1,278 @@
+/*const URL_PEOPLE = "https://swapi.dev/api/people";
+const URL_PLANET = "https://swapi.dev/api/planets";
+const URL_STARSHIPS = "https://swapi.dev/api/starships";
+
+/*window.addEventListener("load", Init);
+
+function Init() {
+  Request(URL_PEOPLE, ShowPeople);
+
+  const people = document.querySelector(".people");
+  people.addEventListener("click", () => {
+    Request(URL_PEOPLE, ShowPeople);
+  })
+
+  const planet = document.querySelector(".planets");
+  planet.addEventListener("click", () => {
+    Request(URL_PLANET, ShowPlanet)
+  })
+
+  const starships = document.querySelector(".starships");
+  starships.addEventListener("click", () => {
+    Request(URL_STARSHIPS, ShowStarships);
+  })
+}
+
+function Request(URL, Callback) {
+  fetch(URL)
+    .then((result) => {
+      return result.json();
+    })
+    .then((data) => {
+      Callback(data);
+    })
+    .catch((err) => console.log(err));
+}
+
+ShowPeople = ({results}) => {
+  console.log(results);
+ 
+    let tr = [];
+    let cells = [];
+  
+  for(i=0;i<10;i++)
+  {
+    //while(cells[i].firstChild){
+      //  cells[i].removeChild(cells[i].lastChild);
+    //}
+    let div = document.createElement("div");
+    div.className="body_slide";
+
+    //let name = document.createElement("p");
+    let height = document.createElement("p");
+    let mass = document.createElement("p");
+    let hair_color = document.createElement("p");
+    let skin_color = document.createElement("p");
+    let eye_color = document.createElement("p");
+    let birth_year = document.createElement("p");
+    let gender = document.createElement("p");
+
+    let name = document.querySelector("#name");
+
+    //div.document.querySelector(name);
+    /*div.appendChild(height);
+     div.appendChild(mass);
+     div.appendChild(hair_color);
+     div.appendChild(skin_color);
+     div.appendChild(eye_color);
+     div.appendChild(birth_year);
+     div.appendChild(gender);
+
+     name.textContent = results[i].name;
+     height.textContent = results[i].height;
+     mass.textContent = results[i].mass;
+     hair_color.textContent = results[i].hair_color;
+     skin_color.textContent = results[i].skin_color;
+     eye_color.textContent = results[i].eye_color;
+     birth_year.textContent = results[i].birth_year;
+     gender.textContent = results[i].gender;
+  }
+};
+
+ShowPlanet = ({results}) => {
+  console.log(results);
+};
+
+ShowStarships = ({results}) => {
+  console.log(results);
+};*/
+
+
+
+
+
+
+
+
+
+
+
+const UrlPeople = "https://swapi.dev/api/people";
+const UrlPlanets = "https://swapi.dev/api/planets";
+const UrlStarships = "https://swapi.dev/api/starships";
+let urlNext;
+let urlPrevious;
+let page = 0;
+let planetStr = "planets";
+let charStr = "characters";
+let shipsStr = "starships";
+let currentImgUrl;
+let currentUrl;
+let currentLocalRequest;
+let requestCount = 0;
+
+window.addEventListener("load", Init);
+
+function Init() {
+  page = 0;
+  currentUrl = UrlPeople;
+  currentImgUrl = charStr;
+  document.getElementById("loader").style.display = "none";
+  Request(UrlPeople, Print);
+}
+
+function GetPeople() {
+  Clear();
+  currentUrl = UrlPeople;
+  page = 0;
+  currentImgUrl = charStr;
+  Request(UrlPeople, Print);
+}
+
+function GetStarship() {
+  Clear();
+  currentUrl = UrlStarships;
+  page = 0;
+  currentImgUrl = shipsStr;
+  Request(UrlStarships, Print);
+}
+
+function GetPlanet() {
+  Clear();
+  currentUrl = UrlPlanets;
+  page = 0;
+  currentImgUrl  = planetStr;
+  Request(UrlPlanets, Print);
+}
+
+function Request (URL, Callback){
+  if(URL === UrlPeople) {
+    currentLocalRequest = "people";
+  }
+  else if(URL === UrlPlanets) {
+    currentLocalRequest = "planet";
+  }
+  else if(URL == UrlStarships) {
+    currentLocalRequest = "ships";
+  }
+
+  document.querySelectorAll(".btn-primary").forEach((element) => {
+    element.style.display = "none";
+  });
+  document.getElementById("loader").style.display = "block";
+
+  const localResult = localStorage.getItem(`${currentLocalRequest}${page}`);
+  if(localResult !== null) {
+    console.log(JSON.parse(localResult))
+    Callback(JSON.parse(localResult));
+    document.getElementById("loader").style.display = "none";
+    document.querySelectorAll(".btn-primary").forEach((element) => {
+      element.style.display = "block";
+    });
+  }
+  else {
+    fetch(URL)
+  .then((result) => {
+    return result.json();
+  })
+  .then((data) => {
+    requestCount++;
+    UpdateCounter();
+    document.getElementById("loader").style.display = "none";
+    document.querySelectorAll(".btn-primary").forEach((element) => {
+      element.style.display = "block";
+    });
+    localStorage.setItem(`${currentLocalRequest}${page}`, JSON.stringify(data));
+    console.log(data)
+    Callback(data);
+  })
+  .catch(err => console.log(err));
+  }
+}
+
+function UpdateCounter() {
+  const counter = document.getElementById("counterId");
+  counter.innerHTML = requestCount;
+}
+
+Next = () => {
+  Clear();
+  page++;
+  currentUrl = urlNext;
+  Request(urlNext, Print);
+}
+
+Prev = () => {
+  Clear();
+  page--;
+  currentUrl = urlPrevious;
+  Request(urlPrevious, Print);
+}
+
+Print = ({results, next, previous}) => {
+
+  urlPrevious = previous;
+  urlNext = next;
+
+  if(next === null) {
+    document.getElementById("btnNext").disabled = true;
+  }
+  else {
+    document.getElementById("btnNext").disabled = false;
+  }
+
+  if(previous === null) {
+    document.getElementById("btnPrev").disabled = true;
+  }
+  else {
+    document.getElementById("btnPrev").disabled = false;
+  }
+  const thead = document.getElementById("tableHead");
+  const tbody = document.getElementById("tableBody");
+  const trHead = document.createElement("tr");
+  
+  Object.keys(results[0]).slice(0, 8).forEach((element) => {
+    const th = document.createElement("th");
+    th.appendChild(document.createTextNode(element));
+    trHead.appendChild(th);
+  });
+  thead.appendChild(trHead);
+
+  results.forEach((element, index) => {
+    const trBody = document.createElement("tr");
+    trBody.addEventListener("click", () => {
+      console.log(trBody);
+    });
+    let img = document.createElement("img");
+
+    let numberItem = index + 1 + page * 10;
+    if(numberItem >= 17) {
+      numberItem++;
+    }
+
+    img.setAttribute("src", `https://starwars-visualguide.com/assets/img/${currentImgUrl}/${numberItem}.jpg`);
+    img.setAttribute("alt", `No image`);
+    img.width = 50;
+    let tdimg = document.createElement("td");
+    tdimg.appendChild(img);
+    trBody.appendChild(tdimg);
+
+    Object.values(element).slice(0, 8).forEach((element1) => {
+      let td = document.createElement("td");
+      td.appendChild(document.createTextNode(element1));
+      trBody.appendChild(td);
+    });
+    tbody.appendChild(trBody);
+  })
+}
+
+function Clear() {
+  let thead = document.getElementById("tableHead");
+  while(thead.firstChild) {
+    thead.removeChild(thead.firstChild);
+  }
+  let tbody = document.getElementById("tableBody");
+  while(tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+}
